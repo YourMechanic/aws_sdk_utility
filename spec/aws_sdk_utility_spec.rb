9418ef632b3f53c6ff31cdff26c8d59b1c38626c
TruptiHosmani <Trupti.hosamani7@gmail.com>
2021-12-03 12:32:15 +0530

    Fix tests on circleci

--- spec/aws_sdk_utility_spec.rb
+++ spec/aws_sdk_utility_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe AwsSdkUtility do
     AwsSdkUtility.amazon_key = 'amazon_key'
     AwsSdkUtility.amazon_access_key = 'amazon_access_key'
     AwsSdkUtility.s3_endpoint = 's3.amazonaws.com'
-    client = Aws::S3::Client.new(stub_responses: true)
+    client = AWS::S3::Client.new(stub_responses: true)
   end
 
   it 'has a version number' do
